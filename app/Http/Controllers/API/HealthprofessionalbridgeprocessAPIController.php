f2beb00dd0e3f0830df98b8bb2934c085815a9c9
Dennis Paulino <dennispaulino08@gmail.com>
2018-10-03 17:45:25 +0100

    NanoSTIMA Bridge - added state parameter to healthProfessionalAPIController createOrUpdate

--- app/Http/Controllers/API/HealthprofessionalbridgeprocessAPIController.php
+++ app/Http/Controllers/API/HealthprofessionalbridgeprocessAPIController.php
@@ -83,9 +83,13 @@ class HealthprofessionalbridgeprocessAPIController extends Controller
             $parametersToStore = ['idHealthProfessional' => $request->idHealthProfessional, 'systemProcessId' => $request->systemProcessId];
            
             if($request->has('idApp'))
-                 $parametersToStore = ['idHealthProfessional' => $request->idHealthProfessional, 'systemProcessId' => $request->systemProcessId,'idApp'=>$request->idApp];
+                array_push ($parametersToStore,['idApp'=>$request->idApp]);
           
-              if ($this->validateValue($request->idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($request->systemProcessId, 'required|numeric')&& $this->validateValue($request->idHealthProfessional, 'required|numeric'))
+            if($request->has('state'))
+                array_push ($parametersToStore,['idApp'=>$request->state]);
+            
+            
+              if ($this->validateValue($request->idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($request->systemProcessId, 'required|numeric')&& $this->validateValue($request->idHealthProfessional, 'required|numeric')&&$this->validateValue($request->state, "required|max:255|regex:[A-Za-z1-9 ]"))
                   $result= Healthprofessionalbridgeprocess::healthProfessionalBridgeProcessCreateModel($parametersToMatch,$parametersToStore);
               else 
                  return response()->json("", 422); 
