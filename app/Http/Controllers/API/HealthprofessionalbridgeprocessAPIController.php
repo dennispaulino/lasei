25300501e0c2d47ca55af66038b39590548a8612
Dennis Paulino <dennispaulino08@gmail.com>
2018-10-03 17:42:17 +0100

    NanoSTIMA Bridge - validate values in HealthProfessional bridge

--- app/Http/Controllers/API/HealthprofessionalbridgeprocessAPIController.php
+++ app/Http/Controllers/API/HealthprofessionalbridgeprocessAPIController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\Healthprofessionalbridgeprocess;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class HealthprofessionalbridgeprocessAPIController extends Controller
 {
@@ -12,19 +13,31 @@ class HealthprofessionalbridgeprocessAPIController extends Controller
     public function index() {
         return response()->json("", 204);
     }
-    public function getSystemProcessIdByIdHealthProfessional(Request $request,$idHealthProfessional) {
-        
-      $idApp = $request->query('idApp', -1);
-      $result= Healthprofessionalbridgeprocess::getSystemProcessIdByIdHealthProfessionalModel($idHealthProfessional,$idApp);
-        if(count($result)>0)
+    public function getSystemProcessIdByIdHealthProfessional(Request $request,$idHealthProfessional) 
+    {
+      
+        $idApp = $request->query('idApp', -1);
+
+        if ($this->validateValue($idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($idHealthProfessional, 'required|numeric'))
+            $result = Healthprofessionalbridgeprocess::getSystemProcessIdByIdHealthProfessionalModel($idHealthProfessional, $idApp);
+        else 
+             return response()->json("", 422);
+
+        if (count($result) > 0)
             return response()->json($result, 200);
         else
-           return response()->json("", 204);
+            return response()->json("", 204);
     }
 
     public function getIdHealthProfessionalBySystemProcessId(Request $request,$systemProcessId) {
-          $idApp = $request->query('idApp', -1);
-        $result= Healthprofessionalbridgeprocess::getIdHealthProfessionalBySystemProcessIdModel($systemProcessId,$idApp);
+         $idApp = $request->query('idApp', -1);
+          
+        if ($this->validateValue($idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($systemProcessId, 'required|numeric'))
+             $result= Healthprofessionalbridgeprocess::getIdHealthProfessionalBySystemProcessIdModel($systemProcessId,$idApp);
+        else 
+             return response()->json("", 422); 
+          
+    
         if(count($result)>0)
            return response()->json($result, 200);
         else
@@ -33,15 +46,25 @@ class HealthprofessionalbridgeprocessAPIController extends Controller
 
     public function getHealthProfessionalBridgeInfoByIdHealthProfessionalAndSystemProcessId(Request $request,$idHealthProfessional,$systemProcessId) {
         $idApp = $request->query('idApp', -1);
-        $result= Healthprofessionalbridgeprocess::getHealthProfessionalBridgeInfoByIdHealthProfessionalAndSystemProcessId($idHealthProfessional,$systemProcessId,$idApp);
-        if(count($result)>0)
+        
+          if ($this->validateValue($idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($systemProcessId, 'required|numeric')&& $this->validateValue($idHealthProfessional, 'required|numeric'))
+              $result= Healthprofessionalbridgeprocess::getHealthProfessionalBridgeInfoByIdHealthProfessionalAndSystemProcessId($idHealthProfessional,$systemProcessId,$idApp);
+         else 
+             return response()->json("", 422); 
+          
+         if(count($result)>0)
            return response()->json($result, 200);
         else
            return response()->json("", 204);
     }
     public function getHealthProfessionalBridgeInfoByIdHealthProfessionalAndExternalProcessId(Request $request,$idHealthProfessional,$externalProcessId) {
         $idApp = $request->query('idApp', -1);
-        $result= Healthprofessionalbridgeprocess::getHealthProfessionalBridgeInfoByIdHealthProfessionalAndExternalProcessId($idHealthProfessional,$externalProcessId,$idApp);
+        
+        if ($this->validateValue($idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($externalProcessId, 'required|numeric')&& $this->validateValue($idHealthProfessional, 'required|numeric'))
+              $result= Healthprofessionalbridgeprocess::getHealthProfessionalBridgeInfoByIdHealthProfessionalAndExternalProcessId($idHealthProfessional,$externalProcessId,$idApp);
+        else 
+             return response()->json("", 422); 
+        
         if(count($result)>0)
            return response()->json($result, 200);
         else
@@ -62,8 +85,11 @@ class HealthprofessionalbridgeprocessAPIController extends Controller
             if($request->has('idApp'))
                  $parametersToStore = ['idHealthProfessional' => $request->idHealthProfessional, 'systemProcessId' => $request->systemProcessId,'idApp'=>$request->idApp];
           
-            $result= Healthprofessionalbridgeprocess::healthProfessionalBridgeProcessCreateModel($parametersToMatch,$parametersToStore);
-         
+              if ($this->validateValue($request->idApp, "required|max:255|regex:[A-Za-z1-9 ]") && $this->validateValue($request->systemProcessId, 'required|numeric')&& $this->validateValue($request->idHealthProfessional, 'required|numeric'))
+                  $result= Healthprofessionalbridgeprocess::healthProfessionalBridgeProcessCreateModel($parametersToMatch,$parametersToStore);
+              else 
+                 return response()->json("", 422); 
+            
             if ($result == 1) 
                 return response()->json("Health Professional Bridge Process was created!", 201);
             else if ($result == 2) 
@@ -73,7 +99,19 @@ class HealthprofessionalbridgeprocessAPIController extends Controller
         return response()->json("", 204);
     }
     
+   
     
-    
+    private static function validateValue($value,$rules)
+    {
+        //needs to be an assoc
+        $assoc = ['value'=>$value];
+
+        $validator = Validator::make($assoc, [
+            'value' => $rules,
+        ]);
+
+        return !$validator->fails();
+
+    }
 
 }
